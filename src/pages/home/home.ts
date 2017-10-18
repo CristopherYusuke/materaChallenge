aa74f2ac8bd21328d41e0f507f1181abafc3bdc2
Cristopher Yusuke <cristopher.yusuke@gmail.com>
2017-10-18 21:29:23 -0200

    alteração na estrutura ionic

---
+++ src/pages/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-home-ionic',
+  templateUrl: 'home-ionic.html'
+})
+export class HomeIonicPage {
+  constructor() {
+
+  }
+}
--- src/pages/user-details/user-details.ts
+++ src/pages/user-details/user-details.ts
@@ -4,10 +4,10 @@ import { NavController, NavParams } from 'ionic-angular';
 
 
 @Component({
-  selector: 'page-item-details',
-  templateUrl: 'item-details.html'
+  selector: 'page-user-details',
+  templateUrl: 'user-details.html'
 })
-export class ItemDetailsPage {
+export class UserDetailsPage {
   selectedItem: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
